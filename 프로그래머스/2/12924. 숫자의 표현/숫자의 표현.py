7c018d0ac0fa9fa9db6c56cde78820cb4fe39462
Riley Byun <51285453+rileybyun@users.noreply.github.com>
2025-03-20 00:07:06 +0900

    [level 2] Title: 숫자의 표현, Time: 0.50 ms, Memory: 9.06 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
@@ -4,13 +4,13 @@ def is_integer(x):
 
 def solution(n):
     # 첫 항이 a이고, 공차가 1인 등차수열의 항이 x개일 때의 합 = x * (a + (x-1)/2)
-    # a가 자연수라면 해당하는 케이스이다.
+    # a가 자연수라면 해당하는 케이스이다. (정수이고 1 이상이어야 함)
     answer = 0
     x = 0
     while True:
         x += 1
         a = n/x - (x-1)/2
         if a < 1: break
-        if is_integer(a) and a >= 1:
+        if is_integer(a):
             answer += 1
     return answer
