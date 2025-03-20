b10bad227b38af4076a95607fcf9342957e9e270
Riley Byun <51285453+rileybyun@users.noreply.github.com>
2025-03-21 02:28:13 +0900

    [level 2] Title: 피보나치 수, Time: 533.17 ms, Memory: 456 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
@@ -1,4 +1,5 @@
 def fibonacci(n):
+    # 재귀함수 (n이 너무 클 경우 부적합)
     if n == 0:
         return 0
     if n == 1:
@@ -10,7 +11,7 @@ def solution(n):
     fibo = [0, 1]
     if n >= 2:
         for i in range(2, n+1):
-            fibo.append(fibo[i-2] + fibo[i-1])
-    # answer = fibonacci(n) % 1234567   # 시간초과
+            fibo.append(fibo[i-2] + fibo[i-1])  # Dynamic Programming (기억하며 풀기)
+    # answer = fibonacci(n) % 1234567   # 시간초과 (함수 스택의 제한)
     answer = fibo[n] % 1234567
     return answer
