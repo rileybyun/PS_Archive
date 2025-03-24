e9667cae7dfd506a77126852318ed7ca2aa1efc8
Riley Byun <51285453+rileybyun@users.noreply.github.com>
2025-03-24 21:51:43 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 0.14 ms, Memory: 9.39 MB -BaekjoonHub

--- 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.py
+++ 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.py
@@ -2,11 +2,7 @@ def solution(data, ext, val_ext, sort_by):
     col_dict = {'code':0, 'date':1, 'maximum':2, 'remain':3}
     ext_idx = col_dict[ext]
     sort_idx = col_dict[sort_by]
-    filtered_data = []
-    
-    for record in data:
-        if record[ext_idx] <= val_ext:
-            filtered_data.append(record)
+    filtered_data = [record for record in data if record[ext_idx] < val_ext]
     
     filtered_data.sort(key = lambda x: x[sort_idx])
     return filtered_data
